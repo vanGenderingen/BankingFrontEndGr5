d08dcf892820069499ca82629f1cd901ad24bc25
FlorisBeentjes <666792@student.inholland.nl>
2023-06-08 15:59:44 +0200

    Changed nav name of products

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -11,7 +11,7 @@
         <li class="nav-item">
           <!-- add a router link to the products page (don't use the a tag!) -->
           <router-link to="/products" class="nav-link" active-class="active"
-            >Overview</router-link
+            >Products</router-link
           >
         </li>
         <li class="nav-item">
