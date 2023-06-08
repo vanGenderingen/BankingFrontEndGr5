bee1c81040402e49d1041cbb897b468dbf0d2cdd
FlorisBeentjes <666792@student.inholland.nl>
2023-06-08 18:59:55 +0200

    Pagination doesnt work fully yet

--- src/components/accounts/userAccountsOverview/UserAccountsOverview.vue
+++ src/components/accounts/userAccountsOverview/UserAccountsOverview.vue
@@ -2,6 +2,17 @@
   <div>
     <Header title="Your Accounts"></Header>
     <div class="account-overview">
+      <div class="pagination">
+        <div class="amount-info">Amount of items to list: </div>
+        <div class="dropdown">
+          <select v-model="itemsPerPage" @change="updateDisplayedAccounts" class="items-per-page-dropdown">
+            <option v-for="option in availableItemsPerPage" :value="option" :key="option">{{ option }}</option>
+          </select>
+        </div>
+        <div class="pagination-info">
+          | Page {{ currentPage }} of {{ totalPages }}
+        </div>
+      </div>
       <table class="account-table">
         <thead>
         <tr>
@@ -13,7 +24,11 @@
         </tr>
         </thead>
         <tbody>
-        <AccountListItem v-for="account in accounts" :key="account.AccountID" :account="account" />
+        <account-list-item
+            v-for="account in displayedAccounts"
+            :key="account.AccountID"
+            :account="account"
+        ></account-list-item>
         </tbody>
       </table>
     </div>
@@ -21,35 +36,79 @@
 </template>
 
 <script>
-import AccountListItem from '@/components/accounts/AccountListItem.vue';
-import axios from 'axios';
-import Header from "@/components/Header.vue";
-export default {
+  import AccountListItem from '@/components/accounts/AccountListItem.vue';
+  import axios from 'axios';
+  import Header from "@/components/Header.vue";
+  export default {
   name: "UserAccountsOverview",
   components: {
-    Header,
-    AccountListItem
+  Header,
+  AccountListItem
+},
+    data() {
+      return {
+        accounts: [],
+        currentPage: 1,
+        itemsPerPage: 20,
+        availableItemsPerPage: [20, 30, 40, 50],
+        searchQuery: '',
+        totalAccounts: 0,
+        totalPages: 0 // Add totalPages property
+      };
+    },
+    methods: {
+      updateDisplayedAccounts() {
+        this.currentPage = 1;
+        this.fetchAccounts();
+      },
+      fetchAccounts() {
+        const userId = 'c0bf2769-79ef-40ca-8410-0c02652028e0'; // Replace with the actual user ID
+        const limit = this.itemsPerPage;
+        const offset = (this.currentPage - 1) * this.itemsPerPage;
+        const searchstrings = this.searchQuery || undefined;
+
+        const url = `http://localhost:8080/accounts/user/${userId}/accounts`;
+        const params = { limit, offset, searchstrings };
+
+        axios
+            .get(url, { params })
+            .then(response => {
+              this.accounts = response.data;
+
+              // Get the total account count from the response headers
+              const totalAccounts = parseInt(response.headers['x-total-count'], 10);
+              this.totalAccounts = totalAccounts || 0;
+
+              // Get the total number of pages from the response headers
+              const totalPages = parseInt(response.headers['x-total-pages'], 10);
+              this.totalPages = totalPages || 0;
+            })
+            .catch(error => {
+              console.error('Error retrieving accounts:', error);
+            });
+      },
+  computed: {
+    totalPages() {
+      return Math.ceil(this.totalAccounts / this.itemsPerPage);
+    },
+    displayedAccounts() {
+      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+      const endIndex = startIndex + this.itemsPerPage;
+
+      return this.accounts.slice(startIndex, endIndex);
+    }
   },
-  data() {
-    return {
-      accounts: []
-    };
+  watch: {
+    currentPage() {
+      this.fetchAccounts();
+    }
   },
   mounted() {
-    const userId = '8b5839e9-fad0-4695-8bdd-c1cd863e6b02'; // Replace with the actual user ID
-    const url = `http://localhost:8080/accounts/user/${userId}/accounts`;
-
-    axios
-        .get(url)
-        .then(response => {
-          this.accounts = response.data;
-        })
-        .catch(error => {
-          console.error('Error retrieving accounts:', error);
-        });
+    this.fetchAccounts();
   }
 };
 </script>
+
 
 <style scoped>
 .account-table {
@@ -58,10 +117,39 @@ export default {
   color: white;
   border-collapse: collapse;
 }
+
 .account-table-head {
   padding-left: 8px;
   padding-top: 10px;
   background-color: #f2f2f2;
   color: black;
+}
+
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+  color: white;
+}
+
+.pagination-info {
+  margin-right: 10px;
+}
+
+.dropdown {
+  position: relative;
+}
+
+.items-per-page-dropdown {
+  color: white;
+  background-color: transparent;
+  border: none;
+  padding: 5px 10px;
+}
+
+.items-per-page-dropdown option {
+  color: black;
+  background-color: white;
 }
 </style>
