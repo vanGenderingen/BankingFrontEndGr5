8ce67c0e30b53aeeb39b99a071674c551666b2ee
666987 <70690069+666987@users.noreply.github.com>
2023-06-08 16:15:10 +0200

    Update Login.vue

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
-    <h2>Login</h2>
+  <div class="login">
+    <h2 class="h2-login">Login</h2>
     <form @submit.prevent="login">
       <input type="email" v-model="email" placeholder="Email" required>
       <input type="password" v-model="password" placeholder="Password" required>
       <button type="submit">Log in</button>
     </form>
   </div>
+
+  <div class="square-container">
+  <div class="square"></div>
+</div>
 </template>
 
 <script>
@@ -43,18 +47,37 @@ export default {
 </script>
 
 <style>
-h2 {
-  color: #333;
+.h2-login {    
+    color: white;
 }
+
+.login {
+  max-width: 33%;
+  margin-top: 5%;
+  margin-left: 33%;
+  text-align: center;
+
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+
 
 input {
   margin-bottom: 10px;
+  width: 50%;
+  text-align: center;
+    margin-left: 25%;
 }
 
 button {
-  background-color: #007bff;
+    width: 25%;
+  background-color: #6f00ff;
   color: #fff;
-  padding: 5px 10px;
+  text-align: center;
+  margin-left: 50%;
   border: none;
   cursor: pointer;
 }
