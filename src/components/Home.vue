e88e70f15b1022b07d94d2719dc1ca31cdd459b2
FlorisBeentjes <666792@student.inholland.nl>
2023-06-08 15:19:08 +0200

    see last commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -6,36 +6,23 @@
         <h1>Welcome to the Red Bank</h1>
       </div>
     </div>
-    <div class="content">
-      <div class="grid">
-        <div class="text-block block1">
-          <h2>Block 1</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div class="text-block block2">
-          <h2>Block 2</h2>
-          <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-        <div class="text-block block3">
-          <h2>Block 3</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div class="text-block block4">
-          <h2>Block 4</h2>
-          <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
-        <div class="text-block block5">
-          <h2>Block 5</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-        <div class="text-block block6">
-          <h2>Block 6</h2>
-          <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        </div>
+    <div class="grid">
+      <div class="text-block block1">
+        <h2>Block 1</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+      <div class="text-block block2">
+        <h2>Block 2</h2>
+        <p>Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+      <div class="text-block block3">
+        <h2>Block 3</h2>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
       </div>
     </div>
   </div>
 </template>
+
 <script>
 export default {
   name: "Home",
@@ -85,16 +72,11 @@ body {
   text-align: center;
 }
 
-.content {
-  display: flex;
-  justify-content: center;
-  padding: 0 150px;
-}
-
 .grid {
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-gap: 50px;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 50px;
+  padding: 0 150px;
 }
 
 .text-block {
@@ -105,39 +87,21 @@ body {
 }
 
 .block1 {
-  border-radius: 0 30px 0 30px;
-  transform: skewX(-10deg);
   grid-column: 1 / span 2;
+  border-radius: 0 30px 30px 0;
+  transform: skewX(-10deg);
 }
 
 .block2 {
-  border-radius: 30px 0 30px 0;
+  grid-column: 2 / span 1;
+  border-radius: 30px 0 0 30px;
   transform: skewX(10deg);
-  grid-column: 2 / span 2;
 }
 
 .block3 {
-  border-radius: 0 30px 0 30px;
+  grid-column: 3 / span 2;
+  border-radius: 0 30px 30px 0;
   transform: skewX(-10deg);
-  grid-column: 1 / span 2;
-}
-
-.block4 {
-  border-radius: 30px 0 30px 0;
-  transform: skewX(10deg);
-  grid-column: 2 / span 2;
-}
-
-.block5 {
-  border-radius: 0 30px 0 30px;
-  transform: skewX(-10deg);
-  grid-column: 1 / span 2;
-}
-
-.block6 {
-  border-radius: 30px 0 30px 0;
-  transform: skewX(10deg);
-  grid-column: 2 / span 2;
 }
 
 .text-block h2 {
