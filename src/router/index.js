07723b46661484ed95490006e3ff93ead416a7a7
FlorisBeentjes <666792@student.inholland.nl>
2023-06-10 14:19:39 +0200

    Changes to router

--- src/router/index.js
+++ src/router/index.js
@@ -11,20 +11,29 @@ import CreateAccount from "@/views/accountViews/CreateAccount.vue";
 import CreateUser from '@/views/userViews/CreateUser.vue';
 import UserOverview from '@/views/userViews/UserOverview.vue';
 import EditUser from '@/views/userViews/EditUser.vue';
+import EditAccount from "@/views/accountViews/EditAccount.vue";
 
 const routes = [
+    //General Routes
   { path: '/', component: Home },
   { path: '/products', component: ProductOverview },
   { path: '/login', component: Login },
+
+    //User Routes
   { path: '/users', component: UsersOverview },
+  { path: '/users/create', component: CreateUser },
+  { path: '/users/:userId', component: UserOverview },
+  { path: '/users/:userId/edit', component: EditUser },
+
+    //Account Routes
   { path: '/accounts', component: GetAllAccounts},
   { path: '/accounts/:accountId', component: SingleAccount},
   { path: '/accounts/user/:userId/accounts', component: UserAccountsOverview },
   { path: '/users/:userId/createAccount', component: CreateAccount },
-  { path: '/accounts/user/:userId/accounts', component: UserAccountsOverview },
-  { path: '/users/create', component: CreateUser },
-  { path: '/users/:userId', component: UserOverview },
-  { path: '/users/:userId/edit', component: EditUser },
+  { path: '/users/:accountId/editAccount', component: EditAccount },
+
+  //Transactions routes
+  //{ path: '/transactions/:accountId/createAccount', component: CreateTransaction },
 ];
 
 const router = createRouter({
