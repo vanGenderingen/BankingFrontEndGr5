fe0d0c3a679e02f2e798abadcd6dc029f4d17e73
Jelle Koomen <668419@student.inholland.nl>
2023-06-23 01:12:41 +0200

    Create ForgotPassword.vue

---
+++ src/components/users/ForgotPassword.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <h1>Forgot Password</h1>
+      <form @submit.prevent="submitForm">
+        <div>
+          <label for="email">Email:</label>
+          <input type="email" id="email" v-model="email" required>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        email: ''
+      }
+    },
+    methods: {
+      submitForm() {
+        const data = { email: this.email };
+        fetch('/forgot-password', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          // Show success message to user
+        })
+        .catch(error => {
+          console.error(error);
+          // Show error message to user
+        });
+      }
+    }
+  }
+  </script>
